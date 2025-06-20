565208167659747cdbc59e19deff622694a3afa7
Amina Ismail Umar <minanmama@gmail.com>
2025-06-20 11:16:34 +0300

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="message">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
